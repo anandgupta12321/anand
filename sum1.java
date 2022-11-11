1ccf91a3548a47e9966cb9be61a480efe9afa0ba
anandgupta12321 <117986775+anandgupta12321@users.noreply.github.com>
2022-11-11 17:01:22 +0530

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sum1
+{
+int x,y,z;
+Scanner sc=new Scanner(System.in);
+void accept(){
+System.out.println("Enter your first number");
+x=sc.nextInt();
+System.out.println("Enter your Second number");
+y=sc.nextInt();
+z=x*y;
+}
+void display(){
+System.out.println("The sum done by anand is "+z".");
+}
+}
